08f283973c702510a374d470530b0c341f968c0c
Hidelbrando Santos <hidelbrandosantos3@gmail.com>
2022-08-16 08:15:11 -0300

    mudança script

--- script.js
+++ script.js
@@ -162,7 +162,9 @@ function updateCart() {
             c('.cart').append(cartItem);
         }
 
-        desconto = subtotal * 0.1;
+        if(subtotal > 30) {
+            desconto = subtotal * 0.1;
+        }
         total = subtotal - desconto;
 
         c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
